204f8cc58fd14e70deb10ec1af6ec9b459bf0983
Moises Molina <mo18022@ues.edu.sv>
2026-08-09 21:52:19 -0600

    Agregar proceso DimUbicacionInicial y correciones menores

--- Structures/Tables/DimUbicacion/DimUbicacion.sql
+++ Structures/Tables/DimUbicacion/DimUbicacion.sql
@@ -4,9 +4,9 @@ go
 DROP TABLE IF EXISTS DimUbicacion
 
 CREATE TABLE DimUbicacion(
-	UbicacionKey INT Primary Key,
+	UbicacionKey INT Primary Key Identity(1,1),
 	DireccionId INT NOT NULL,
-	DireccionCompleta VARCHAR(500),
+	DireccionCompleta VARCHAR(500) NOT NULL,
 	Pais Varchar(100) NOT NULL,
 	Ciudad VARCHAR(100) NOT NULL,
 	Departamento VARCHAR(100) NOT NULL,
